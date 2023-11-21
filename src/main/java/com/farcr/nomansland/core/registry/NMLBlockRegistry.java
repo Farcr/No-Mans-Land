04f762cb87a796da3f6c0a0f26fe942a978f4d6b
Farcr <slender.farris@gmail.com>
2023-11-21 02:08:24 +0100

    yea

---
+++ src/main/java/com/farcr/nomansland/core/registry/NMLBlockRegistry.java
@@ -0,0 +1,44 @@
+package com.farcr.nomansland.core.registry;
+
+import com.farcr.nomansland.core.NoMansLand;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class NMLBlockRegistry {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(NoMansLand.MODID);
+
+    public static final DeferredBlock<Block> PINE_PLANKS = registerBlock("pine_planks",() -> new Block(Block.Properties.copy(Blocks.OAK_PLANKS)));
+    public static final DeferredBlock<Block> PINE_STAIRS = registerBlock("pine_stairs",() -> new StairBlock(() ->  NMLBlockRegistry.PINE_PLANKS.get()
+            .defaultBlockState(),Block.Properties.copy(NMLBlockRegistry.PINE_PLANKS.get())));
+    public static final DeferredBlock<Block> PINE_SLAB = registerBlock("pine_slab",() -> new SlabBlock(Block.Properties.copy(NMLBlockRegistry.PINE_PLANKS.get())));
+
+
+
+
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> DeferredItem<Item> registerBlockItem(String name, DeferredBlock<T> block) {
+        return NMLItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+        NoMansLand.LOGGER.info("Registering NoMansLandBlocks for" + NoMansLand.MODID);
+    }
+
+}
---
+++ src/main/java/com/farcr/nomansland/core/registry/NMLItemRegistry.java
@@ -0,0 +1,8 @@
+package com.farcr.nomansland.core.registry;
+
+import com.farcr.nomansland.core.NoMansLand;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class NMLItemRegistry {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(NoMansLand.MODID);
+}
