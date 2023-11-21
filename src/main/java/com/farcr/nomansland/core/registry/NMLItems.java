bbb336b02f80bc269f1119b07c160c646382d12b
Farcr <slender.farris@gmail.com>
2023-11-21 17:29:11 +0100

    we restart this

--- src/main/java/com/farcr/nomansland/core/registry/NMLItems.java
+++
@@ -1,9 +0,0 @@
-package com.farcr.nomansland.core.registry;
-
-import com.farcr.nomansland.core.NoMansLand;
-import net.neoforged.neoforge.registries.DeferredRegister;
-
-public class NMLItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(NoMansLand.MODID);
-
-}
