709fa5c769771ebbe6d399726f168b4be2ee3484
Farcr <slender.farris@gmail.com>
2023-12-06 17:52:40 +0100

    Start of Trimmed Plank Blockstates + update bamboo textures

--- src/main/java/com/farcr/nomansland/core/content/block/TrimmedPlankBlock.java
+++ src/main/java/com/farcr/nomansland/core/content/block/TrimmedPlankBlock.java
@@ -2,9 +2,20 @@ package com.farcr.nomansland.core.content.block;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 public class TrimmedPlankBlock extends Block {
+
+    public static final BooleanProperty UP = BlockStateProperties.UP;
+    public static final BooleanProperty DOWN = BlockStateProperties.DOWN;
     public TrimmedPlankBlock(BlockBehaviour.Properties pProperties) {
         super(pProperties);
+    }
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(UP);
+        pBuilder.add(DOWN);
     }
 }
