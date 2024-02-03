c80b50fa79fc4bfbf23d60289a3b93c21894a5db
MonkyMonka <daniyaldanish11dd@gmail.com>
2024-02-03 14:11:47 -0500

    Remains no longer have Gravity, Falling Particles

--- src/main/java/com/farcr/nomansland/core/content/block/RemainsBlock.java
+++ src/main/java/com/farcr/nomansland/core/content/block/RemainsBlock.java
@@ -1,13 +1,31 @@
 package com.farcr.nomansland.core.content.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BrushableBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BrushableBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class RemainsBlock extends BrushableBlock {
     public RemainsBlock(Block pTurnsInto, Properties pProperties, SoundEvent pBrushSound, SoundEvent pBrushCompletedSound) {
         super(pTurnsInto, pProperties, pBrushSound, pBrushCompletedSound);
     }
+
+    @Override
+    public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+        BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+        if (blockentity instanceof BrushableBlockEntity brushableblockentity) {
+            brushableblockentity.checkReset();
+        }
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRandom) { return; }
 
     //WE'LL PUT BURIED SPAWNING IN HERE!!
     //also gotta make it not be subject to gravity
