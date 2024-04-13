a874ab5793f756ab1077e942302daab7bd2f299a
Farcr <slender.farris@gmail.com>
2024-04-13 13:39:58 +0200

    jungle tweaks and alembic compat fix

--- src/main/java/com/farcr/nomansland/core/registry/NMLLootModifiers.java
+++ src/main/java/com/farcr/nomansland/core/registry/NMLLootModifiers.java
@@ -16,6 +16,8 @@ public class NMLLootModifiers {
     public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_ITEM =
             LOOT_MODIFIER_SERIALIZERS.register("add_item", AddItemModifier.CODEC);
 
+    //TODO: ADD FUCKING MOD CONDITIONAL LOOT MODIFIER
+
 
     public static void register(IEventBus eventBus) {
         LOOT_MODIFIER_SERIALIZERS.register(eventBus);
