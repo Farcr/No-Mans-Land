f98cd251e5bc2949774a958174658f5b70678b99
Farcr <slender.farris@gmail.com>
2024-05-02 00:44:17 +0200

    biome slice starter setup

---
+++ src/main/java/com/farcr/nomansland/core/content/world/NMLBiomeSlices.java
@@ -0,0 +1,16 @@
+package com.farcr.nomansland.core.content.world;
+
+import com.farcr.nomansland.core.NoMansLand;
+import com.teamabnormals.blueprint.common.world.modification.ModdedBiomeSlice;
+import com.teamabnormals.blueprint.core.registry.BlueprintDataPackRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+public class NMLBiomeSlices {
+
+    public static final ResourceKey<ModdedBiomeSlice> AUTUMNAL = createKey("autumnal");
+
+    public static ResourceKey<ModdedBiomeSlice> createKey(String name) {
+        return ResourceKey.create(BlueprintDataPackRegistries.MODDED_BIOME_SLICES, new ResourceLocation(NoMansLand.MODID, name));
+    }
+}
