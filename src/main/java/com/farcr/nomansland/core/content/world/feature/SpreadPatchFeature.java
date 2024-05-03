410e260043bd748f9a426de5151f7d91c1b5bded
Farcr <slender.farris@gmail.com>
2024-05-04 01:52:37 +0200

    Spread Patch Feature console issue fix

--- src/main/java/com/farcr/nomansland/core/content/world/feature/SpreadPatchFeature.java
+++ src/main/java/com/farcr/nomansland/core/content/world/feature/SpreadPatchFeature.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
 
+//Thank you greatest cappin for helping with this
 public class SpreadPatchFeature extends Feature<RandomPatchConfiguration> {
     public SpreadPatchFeature(Codec<RandomPatchConfiguration> pCodec) {
         super(pCodec);
@@ -24,13 +25,45 @@ public class SpreadPatchFeature extends Feature<RandomPatchConfiguration> {
         int xzSpread = config.xzSpread();
         int ySpread = config.ySpread();
 
-        for (int i = 0; i <tries ; i++) {
+        BlockPos.MutableBlockPos pos = origin.mutable();
+        for (int i = 0; i < tries; i++) {
             int x = (int) (origin.getX() + random.nextGaussian() * xzSpread);
             int z = (int) (origin.getZ() + random.nextGaussian() * xzSpread);
             int y = origin.getY() + random.nextInt(-ySpread, ySpread);
-            BlockPos Pos = new BlockPos(x,y,z);
-            config.feature().value().place(level, pContext.chunkGenerator(), random, Pos);
+            pos.set(x, y, z);
+
+            if (this.shouldPlace(level, random, pos)) {
+                config.feature().value().place(level, pContext.chunkGenerator(), random, pos);
+            }
         }
         return true;
+    }
+    // determines if we should place at a block - if it's not yet loaded, we don't place anything there!!
+    // also does some dithering so that it'll smoothly fade if it encounters an unloaded chunk,
+    // rather than a really ugly hard cutoff
+    // .
+    // this might be really laggy. give it a try to make sure it's not.
+    private boolean shouldPlace(WorldGenLevel level, RandomSource random, BlockPos pos) {
+        // exit early if this chunk doesn't yet exist
+        if (!level.hasChunkAt(pos)) {
+            return false;
+        }
+
+        // dithering
+        int successfulTries = 0;
+        BlockPos.MutableBlockPos checkPos = pos.mutable();
+        for (int i = 0; i < 8; i++) {
+            checkPos.offset((int) (random.nextGaussian() * 4), 0, (int) (random.nextGaussian() * 4));
+            if (level.hasChunkAt(pos)) {
+                successfulTries++;
+            }
+        }
+
+        // totally arbitrary threshold
+        if (successfulTries > 5) {
+            return true;
+        }
+
+        return false;
     }
 }
