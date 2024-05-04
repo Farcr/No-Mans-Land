cdd29d2585f6d3b257bf760004d695d0565e96dc
Farcr <slender.farris@gmail.com>
2024-05-04 21:33:49 +0200

    remove NMLLootModifiers as it's no longer needed

--- src/main/java/com/farcr/nomansland/core/NoMansLand.java
+++ src/main/java/com/farcr/nomansland/core/NoMansLand.java
@@ -2,7 +2,6 @@ package com.farcr.nomansland.core;
 
 import com.farcr.nomansland.core.content.entity.client.NMLBoatRenderer;
 import com.farcr.nomansland.core.registry.*;
-import com.farcr.nomansland.core.registry.NMLLootModifiers;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.level.block.Blocks;
@@ -43,8 +42,6 @@ public class NoMansLand {
         NMLFeatures.FEATURES.register(modEventBus);
 
         modEventBus.addListener(NMLItems::addCreative);
-
-        NMLLootModifiers.LOOT_MODIFIER_SERIALIZERS.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
