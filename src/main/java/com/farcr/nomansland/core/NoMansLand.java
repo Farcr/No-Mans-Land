486fc431bf226a47d076746521751d510b31d780
MonkyMonka <daniyaldanish11dd@gmail.com>
2024-05-07 04:58:19 -0400

    Added applicable compostability

--- src/main/java/com/farcr/nomansland/core/NoMansLand.java
+++ src/main/java/com/farcr/nomansland/core/NoMansLand.java
@@ -5,6 +5,7 @@ import com.farcr.nomansland.core.registry.*;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -70,6 +71,49 @@ public class NoMansLand {
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(NMLBlocks.BRANCHY_SUCCULENT.getId(), NMLBlocks.POTTED_BRANCHY_SUCCULENT);
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(NMLBlocks.PRICKLY_SUCCULENT.getId(), NMLBlocks.POTTED_PRICKLY_SUCCULENT);
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(NMLBlocks.BARREL_CACTUS.getId(), NMLBlocks.POTTED_BARREL_CACTUS);
+        });
+        event.enqueueWork(() -> {
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.YELLOW_BIRCH_LEAVES.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.AUTUMNAL_OAK_LEAVES.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.PALE_CHERRY_LEAVES.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.RED_MAPLE_LEAVES.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.MAPLE_LEAVES.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.PINE_LEAVES.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.WALNUT_LEAVES.get(), 0.30f);
+
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.YELLOW_BIRCH_SAPLING.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.AUTUMNAL_OAK_SAPLING.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.PALE_CHERRY_SAPLING.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.RED_MAPLE_SAPLING.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.MAPLE_SAPLING.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.PINE_SAPLING.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.WALNUT_SAPLING.get(), 0.30f);
+
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.GRASS_SPROUTS.get(), 0.10f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.OAT_GRASS.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.FIDDLEHEAD.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.CATTAIL.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.ACONITE.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.PINK_LUPINE.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.RED_LUPINE.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.YELLOW_FLOWERBED.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.BLUE_LUPINE.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.AUTUMN_CROCUS.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.WILD_MINT.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.RAFFLESIA.get(), 0.85f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.BARREL_CACTUS.get(), 0.50f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.HEARTY_SUCCULENT.get(), 0.50f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.BRANCHY_SUCCULENT.get(), 0.50f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.PRICKLY_SUCCULENT.get(), 0.50f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.CUT_VINE.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.CUT_SUGAR_CANE.get(), 0.30f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.CLOVER_PATCH.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.VIOLET_FLOWERBED.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.WHITE_FLOWERBED.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.RED_FLOWERBED.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.YELLOW_FLOWERBED.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(NMLBlocks.BLUE_FLOWERBED.get(), 0.65f);
+
         });
     }
 
