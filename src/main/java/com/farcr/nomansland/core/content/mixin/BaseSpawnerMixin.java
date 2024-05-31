7352678cd29808070948661733c7fa08aed18796
Tazer <yaseenelgamal20094@gmail.com>
2024-05-31 03:27:56 +0300

    made spawners produce malevolent flames

---
+++ src/main/java/com/farcr/nomansland/core/content/mixin/BaseSpawnerMixin.java
@@ -0,0 +1,22 @@
+package com.farcr.nomansland.core.content.mixin;
+
+import com.farcr.nomansland.core.registry.NMLParticleTypes;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.level.BaseSpawner;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(BaseSpawner.class)
+public class BaseSpawnerMixin {
+
+    // clienttick
+    @ModifyArg(method = "clientTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"), index = 0)
+    private ParticleOptions injected(ParticleOptions particle) {
+        if (particle == ParticleTypes.FLAME) {
+            return particle = (ParticleOptions) NMLParticleTypes.MALEVOLENT_FLAME.get();
+        }
+        return particle;
+    }
+}
