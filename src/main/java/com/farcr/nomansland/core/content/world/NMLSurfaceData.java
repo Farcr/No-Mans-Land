70c9e1a667e534a26067d827b29bd11881d12cf7
Tazer <yaseenelgamal20094@gmail.com>
2024-09-10 04:41:37 +0300

    fix them hehe

--- src/main/java/com/farcr/nomansland/core/content/world/NMLSurfaceData.java
+++ src/main/java/com/farcr/nomansland/core/content/world/NMLSurfaceData.java
@@ -2,6 +2,7 @@ package com.farcr.nomansland.core.content.world;
 
 import com.farcr.nomansland.core.registry.NMLBiomes;
 import com.farcr.nomansland.core.registry.NMLBlocks;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.Noises;
@@ -11,11 +12,37 @@ import net.minecraft.world.level.levelgen.VerticalAnchor;
 public class NMLSurfaceData {
     private static final SurfaceRules.RuleSource COARSE_DIRT = makeStateRule(Blocks.COARSE_DIRT);
     private static final SurfaceRules.RuleSource PODZOL = makeStateRule(Blocks.PODZOL);
+    private static final SurfaceRules.RuleSource AIR = makeStateRule(Blocks.AIR);
+    private static final SurfaceRules.RuleSource ICE = makeStateRule(Blocks.ICE);
+    private static final SurfaceRules.RuleSource WATER = makeStateRule(Blocks.WATER);
+
     protected static SurfaceRules.RuleSource makeRules() {
-        return SurfaceRules.sequence(
+        SurfaceRules.ConditionSource surfacerules$conditionsource7 = SurfaceRules.waterBlockCheck(-1, 0);
+        SurfaceRules.ConditionSource surfacerules$conditionsource11 = SurfaceRules.isBiome(Biomes.FROZEN_OCEAN, Biomes.DEEP_FROZEN_OCEAN);
+        SurfaceRules.ConditionSource surfacerules$conditionsource10 = SurfaceRules.hole();
+        SurfaceRules.ConditionSource surfacerules$conditionsource8 = SurfaceRules.waterBlockCheck(0, 0);
+
+        return SurfaceRules.ifTrue(
+                SurfaceRules.ON_FLOOR,
                 SurfaceRules.ifTrue(
-                        SurfaceRules.isBiome(NMLBiomes.MAPLE_FOREST),
-                        SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.75), COARSE_DIRT), SurfaceRules.ifTrue(surfaceNoiseAbove(-0.95), PODZOL))
+                        surfacerules$conditionsource7,
+                        SurfaceRules.sequence(
+                                SurfaceRules.ifTrue(
+                                        surfacerules$conditionsource11,
+                                        SurfaceRules.ifTrue(
+                                                surfacerules$conditionsource10,
+                                                SurfaceRules.sequence(
+                                                        SurfaceRules.ifTrue(surfacerules$conditionsource8, AIR), SurfaceRules.ifTrue(SurfaceRules.temperature(), ICE), WATER
+                                                )
+                                        )
+                                ),
+                                SurfaceRules.sequence(
+                                        SurfaceRules.ifTrue(
+                                                SurfaceRules.isBiome(NMLBiomes.AUTUMNAL_FOREST),
+                                                SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.75), COARSE_DIRT), SurfaceRules.ifTrue(surfaceNoiseAbove(-0.95), PODZOL))
+                                        )
+                                )
+                        )
                 )
         );
     }
