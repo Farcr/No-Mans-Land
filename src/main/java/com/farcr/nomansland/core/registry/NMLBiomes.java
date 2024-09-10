70e4d786e80c42d21d5766941e1cc411bcf61df3
Farcr <slender.farris@gmail.com>
2024-09-10 02:37:59 +0200

    Autumnal forest tweaks! It doesn't generate tho why

--- src/main/java/com/farcr/nomansland/core/registry/NMLBiomes.java
+++ src/main/java/com/farcr/nomansland/core/registry/NMLBiomes.java
@@ -7,11 +7,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 
 public class NMLBiomes {
+
+    //Surface
     public static final ResourceKey<Biome> AUTUMNAL_FOREST = createKey("autumnal_forest");
-    public static final ResourceKey<Biome> CAVES = createKey("caves");
-    public static final ResourceKey<Biome> HIGHLAND = createKey("highlands");
     public static final ResourceKey<Biome> MAPLE_FOREST = createKey("maple_forest");
     public static final ResourceKey<Biome> OLD_GROWTH_FOREST = createKey("old_growth_forest");
+
+    //Underground
+
+    public static final ResourceKey<Biome> CAVES = createKey("caves");
 
 
     public static ResourceKey<Biome> createKey(String name) {
