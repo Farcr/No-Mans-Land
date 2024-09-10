6a232259a82d7eee062909285b39d318cd684aec
Farcr <slender.farris@gmail.com>
2024-09-10 04:18:59 +0200

    surface rules replacing goofy ahh features

--- src/main/java/com/farcr/nomansland/core/content/world/NMLSurfaceData.java
+++ src/main/java/com/farcr/nomansland/core/content/world/NMLSurfaceData.java
@@ -17,33 +17,27 @@ public class NMLSurfaceData {
     private static final SurfaceRules.RuleSource WATER = makeStateRule(Blocks.WATER);
 
     protected static SurfaceRules.RuleSource makeRules() {
-        SurfaceRules.ConditionSource surfacerules$conditionsource7 = SurfaceRules.waterBlockCheck(-1, 0);
-        SurfaceRules.ConditionSource surfacerules$conditionsource11 = SurfaceRules.isBiome(Biomes.FROZEN_OCEAN, Biomes.DEEP_FROZEN_OCEAN);
-        SurfaceRules.ConditionSource surfacerules$conditionsource10 = SurfaceRules.hole();
-        SurfaceRules.ConditionSource surfacerules$conditionsource8 = SurfaceRules.waterBlockCheck(0, 0);
 
         return SurfaceRules.ifTrue(
                 SurfaceRules.ON_FLOOR,
-                SurfaceRules.ifTrue(
-                        surfacerules$conditionsource7,
                         SurfaceRules.sequence(
-                                SurfaceRules.ifTrue(
-                                        surfacerules$conditionsource11,
-                                        SurfaceRules.ifTrue(
-                                                surfacerules$conditionsource10,
-                                                SurfaceRules.sequence(
-                                                        SurfaceRules.ifTrue(surfacerules$conditionsource8, AIR), SurfaceRules.ifTrue(SurfaceRules.temperature(), ICE), WATER
-                                                )
-                                        )
-                                ),
-                                SurfaceRules.sequence(
-                                        SurfaceRules.ifTrue(
-                                                SurfaceRules.isBiome(NMLBiomes.AUTUMNAL_FOREST),
-                                                SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.75), COARSE_DIRT), SurfaceRules.ifTrue(surfaceNoiseAbove(-0.95), PODZOL))
-                                        )
-                                )
-                        )
-                )
+                //Autumnal Forest
+                SurfaceRules.ifTrue(
+                        SurfaceRules.isBiome(NMLBiomes.AUTUMNAL_FOREST),
+                        SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.75), COARSE_DIRT), SurfaceRules.ifTrue(surfaceNoiseAbove(-0.95), PODZOL))),
+                //Old Growth Forest
+                SurfaceRules.ifTrue(
+                        SurfaceRules.isBiome(NMLBiomes.OLD_GROWTH_FOREST),
+                        SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.5), COARSE_DIRT))),
+                //Dark Forest
+                SurfaceRules.ifTrue(
+                        SurfaceRules.isBiome(Biomes.DARK_FOREST),
+                        SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.5), COARSE_DIRT))),
+                //Jungle
+                SurfaceRules.ifTrue(
+                        SurfaceRules.isBiome(Biomes.JUNGLE),
+                        SurfaceRules.sequence(SurfaceRules.ifTrue(surfaceNoiseAbove(1.5), COARSE_DIRT)))
+            )
         );
     }
 
