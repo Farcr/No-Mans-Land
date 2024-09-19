988627f0c735c71c3d3d6fe3ad5ecf77e5d70df1
exoplanetary <kaden_ostendorf@yahoo.com>
2024-09-18 18:25:25 -0600

    fixed leaf decay issue + increased leaf chances

--- src/main/java/com/farcr/nomansland/core/content/world/feature/foliageplacers/PineFoliagePlacer.java
+++ src/main/java/com/farcr/nomansland/core/content/world/feature/foliageplacers/PineFoliagePlacer.java
@@ -20,8 +20,8 @@ public class PineFoliagePlacer extends FoliagePlacer {
     public static final MapCodec<PineFoliagePlacer> CODEC = RecordCodecBuilder.mapCodec((instance) -> foliagePlacerParts(instance).apply(instance, PineFoliagePlacer::new));
 
     public Set<BlockPos> leafPositions;
-    public Set<BlockPos> leafPositions50;
-    public Set<BlockPos> leafPositions20;
+    public Set<BlockPos> leafPositions30;
+    public Set<BlockPos> leafPositions10;
 
     public PineFoliagePlacer(IntProvider radius, IntProvider offset) {
         super(radius, offset);
@@ -32,52 +32,49 @@ public class PineFoliagePlacer extends FoliagePlacer {
     }
 
     protected void createFoliage(@NotNull LevelSimulatedReader level, @NotNull FoliageSetter blockSetter, @NotNull RandomSource random, @NotNull TreeConfiguration config, int maxFreeTreeHeight, @NotNull FoliageAttachment attachment, int foliageHeight, int foliageRadius, int foliageOffset) {
-        /*for (int j = foliageOffset; j >= foliageOffset - foliageHeight; --j) {
-            this.placeLeavesRow(reader, blockSetter, random, config, attachment.pos(), 2, j, attachment.doubleTrunk());
-        }*/
         this.leafPositions = Sets.newHashSet();
-        this.leafPositions50 = Sets.newHashSet();
-        this.leafPositions20 = Sets.newHashSet();
+        this.leafPositions30 = Sets.newHashSet();
+        this.leafPositions10 = Sets.newHashSet();
 
-        int numCanopies = (foliageHeight - 4) / 2;
+        int numCanopies = (foliageHeight - 5) / 2;
         // Add tree topper
         for (int di = 0; di < 4; di++) {
             Direction d = Direction.from2DDataValue(di);
+            this.leafPositions.add(attachment.pos().below(2).relative(d));
+            this.leafPositions.add(attachment.pos().below(2).relative(d).relative(d.getClockWise()));
+            this.leafPositions.add(attachment.pos().below().relative(d));
+            this.leafPositions10.add(attachment.pos().below().relative(d).relative(d.getClockWise()));
             this.leafPositions.add(attachment.pos().relative(d));
-            this.leafPositions.add(attachment.pos().relative(d).relative(d.getClockWise()));
-            this.leafPositions.add(attachment.pos().above().relative(d));
-            this.leafPositions20.add(attachment.pos().above().relative(d).relative(d.getClockWise()));
-            this.leafPositions.add(attachment.pos().above(2).relative(d));
         }
+        this.leafPositions.add(attachment.pos());
+        this.leafPositions.add(attachment.pos().above());
         this.leafPositions.add(attachment.pos().above(2));
-        this.leafPositions.add(attachment.pos().above(3));
-        this.leafPositions.add(attachment.pos().above(4));
-        this.leafPositions20.add(attachment.pos().above(5));
+        this.leafPositions10.add(attachment.pos().above(3));
 
         for (int i = 0; i < (Math.min(numCanopies, 3)); i++) {
-            placeSmallLayer(attachment.pos().below(2 + 2*i));
+            placeSmallLayer(attachment.pos().below(4 + 2*i));
         }
         if (numCanopies >= 3) {
             for (int i = 0; i < numCanopies - 3; i++) {
-                placeLargeLayer(attachment.pos().below(8 + 2*i));
+                placeLargeLayer(attachment.pos().below(10 + 2*i));
             }
         }
 
-        this.leafPositions.add(attachment.pos().below(1 + 2*numCanopies).north());
-        this.leafPositions.add(attachment.pos().below(1 + 2*numCanopies).east());
-        this.leafPositions.add(attachment.pos().below(1 + 2*numCanopies).south());
-        this.leafPositions.add(attachment.pos().below(1 + 2*numCanopies).west());
+        this.leafPositions.add(attachment.pos().below(3 + 2*numCanopies).north());
+        this.leafPositions.add(attachment.pos().below(3 + 2*numCanopies).east());
+        this.leafPositions.add(attachment.pos().below(3 + 2*numCanopies).south());
+        this.leafPositions.add(attachment.pos().below(3 + 2*numCanopies).west());
 
         // Place the foliage
         for (BlockPos leafPos : leafPositions) {
             tryPlaceLeaf(level, blockSetter, random, config, leafPos);
         }
-        for (BlockPos leafPos : leafPositions20) {
-            if (random.nextInt(5) > 0)
+        for (BlockPos leafPos : leafPositions10) {
+            if (random.nextFloat() > 0.1)
                 tryPlaceLeaf(level, blockSetter, random, config, leafPos);
         }
-        for (BlockPos leafPos : leafPositions50) {
-            if (random.nextInt(2) > 0)
+        for (BlockPos leafPos : leafPositions30) {
+            if (random.nextFloat() > 0.3)
                 tryPlaceLeaf(level, blockSetter, random, config, leafPos);
         }
     }
@@ -98,9 +95,9 @@ public class PineFoliagePlacer extends FoliagePlacer {
 
         for (int di = 0; di < 4; di++) {
             Direction d = Direction.from2DDataValue(di);
-            this.leafPositions20.add(localOrigin.relative(d, 3));
-            this.leafPositions50.add(localOrigin.relative(d, 3).relative(d.getClockWise()));
-            this.leafPositions50.add(localOrigin.relative(d, 3).relative(d.getCounterClockWise()));
+            this.leafPositions10.add(localOrigin.relative(d, 3));
+            this.leafPositions30.add(localOrigin.relative(d, 3).relative(d.getClockWise()));
+            this.leafPositions30.add(localOrigin.relative(d, 3).relative(d.getCounterClockWise()));
             this.leafPositions.add(localOrigin.above().relative(d, 2));
         }
     }
@@ -115,8 +112,8 @@ public class PineFoliagePlacer extends FoliagePlacer {
         for (int di = 0; di < 4; di++) {
             Direction d = Direction.from2DDataValue(di);
             this.leafPositions.add(localOrigin.relative(d, 2));
-            this.leafPositions20.add(localOrigin.relative(d, 2).relative(d.getClockWise()));
-            this.leafPositions20.add(localOrigin.relative(d, 2).relative(d.getCounterClockWise()));
+            this.leafPositions10.add(localOrigin.relative(d, 2).relative(d.getClockWise()));
+            this.leafPositions10.add(localOrigin.relative(d, 2).relative(d.getCounterClockWise()));
             this.leafPositions.add(localOrigin.above().relative(d, 1));
         }
     }
