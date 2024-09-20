8c59c6b5e01c8b7f7b361e7514e6be4de54e91a8
Farcr <slender.farris@gmail.com>
2024-09-20 02:13:36 +0200

    wip shelf mushroom generation

--- src/main/java/com/farcr/nomansland/core/content/block/ShelfMushroomBlock.java
+++ src/main/java/com/farcr/nomansland/core/content/block/ShelfMushroomBlock.java
@@ -1,9 +1,12 @@
 package com.farcr.nomansland.core.content.block;
 
+import com.farcr.nomansland.core.registry.NMLTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.BaseCoralWallFanBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -12,12 +15,18 @@ public class ShelfMushroomBlock extends BaseCoralWallFanBlock {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, Boolean.valueOf(false)));
     }
-//TODO: Change bounding box
+
+    public static boolean isAcceptableNeighbour(BlockGetter blockReader, BlockPos neighborPos) {
+        BlockState blockstate = blockReader.getBlockState(neighborPos);
+        return blockstate.is(BlockTags.LOGS) || blockstate.is(NMLTags.MUSHROOM_BLOCKS);
+    }
+
+    //TODO: Change bounding box
     @Override
     protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
         Direction direction = state.getValue(FACING);
         BlockPos blockpos = pos.relative(direction.getOpposite());
         BlockState blockstate = level.getBlockState(blockpos);
-        return blockstate.is(BlockTags.LOGS);
+        return blockstate.is(BlockTags.LOGS) || blockstate.is(NMLTags.MUSHROOM_BLOCKS);
     }
 }
