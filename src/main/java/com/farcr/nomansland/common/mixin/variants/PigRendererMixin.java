29fc102b6ac9eb8662913b33ba6c9f9d6079ad21
Tazer <yaseenelgamal20094@gmail.com>
2024-09-28 23:05:07 +0300

    cutie piggy

--- src/main/java/com/farcr/nomansland/common/mixin/variants/PigRendererMixin.java
+++ src/main/java/com/farcr/nomansland/common/mixin/variants/PigRendererMixin.java
@@ -1,6 +1,7 @@
 package com.farcr.nomansland.common.mixin.variants;
 
 import com.farcr.nomansland.common.entity.variant.PigVariant;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.PigRenderer;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
@@ -11,17 +12,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PigRenderer.class)
 public abstract class PigRendererMixin  {
-
-    @Shadow public abstract ResourceLocation getTextureLocation(Pig entity);
-
     @Inject(method = "getTextureLocation(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/resources/ResourceLocation;", at = @At("RETURN"), cancellable = true)
     private void getTextureLocation(Entity entity, CallbackInfoReturnable<ResourceLocation> cir) {
         PigVariant variant = ((VariantHolder<Holder<PigVariant>>)entity).getVariant().value();
         Pig pig = (Pig) entity;
         ResourceLocation texture = pig.isBaby() ? variant.babyTexture() : variant.texture();
-        cir.setReturnValue(texture.withPath((path) -> "textures/" + path + ".png"));    }
+        cir.setReturnValue(texture.withPath((path) -> "textures/" + path + ".png"));
+    }
 }
--- src/main/java/com/farcr/nomansland/common/mixin/variants/QuadrupedModelMixin.java
+++ src/main/java/com/farcr/nomansland/common/mixin/variants/QuadrupedModelMixin.java
@@ -1,30 +1,34 @@
 package com.farcr.nomansland.common.mixin.variants;
 
+import com.farcr.nomansland.client.model.NMLPigModel;
 import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.Pig;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(QuadrupedModel.class)
 public class QuadrupedModelMixin<T extends Entity> {
-    @Shadow @Final protected ModelPart head;
+    @Unique
+    protected ModelPart root;
 
-    @Shadow @Final protected ModelPart rightHindLeg;
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void init(ModelPart root, boolean scaleHead, float babyYHeadOffset, float babyZHeadOffset, float babyHeadScale, float babyBodyScale, int bodyYOffset, CallbackInfo ci) {
+        this.root = root;
+    }
 
-    @Shadow @Final protected ModelPart leftHindLeg;
-
-    @Shadow @Final protected ModelPart rightFrontLeg;
-
-    @Shadow @Final protected ModelPart leftFrontLeg;
-
-    @Shadow @Final protected ModelPart body;
-
-    @Inject(method = "setupAnim", at = @At("TAIL"))
+    @Inject(method = "setupAnim", at = @At("HEAD"), cancellable = true)
     protected void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if (entity instanceof Pig pig) {
+            NMLPigModel.doRotations(pig, root, limbSwing, limbSwingAmount, netHeadYaw, headPitch);
+            ci.cancel();
+        }
     }
 }
